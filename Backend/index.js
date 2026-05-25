761b7f311a095a8dae3ecbe5fcae540b94b83163
Tejas <tejasspatell2@gmail.com>
2026-05-25 16:07:03 +0530

    building docuMind

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const MySQLStore = require("express-mysql-session")(session);
 const cors = require("cors");
 const helmetConfig = require("./middleware/helmet");
-const db = require("./config/db");
 const chatRoutes = require("./routers/chatRoutes");
 const authRoutes = require("./routers/authroutes");
 const cookieParser = require("cookie-parser");
@@ -13,11 +9,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-require("./config/passport");
 require("./models/user");
-require("./models/chat_history");
 
-app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -29,26 +22,6 @@ app.use(
     credentials: true,
   })
 );
-
-const sessionStore = new MySQLStore({}, db);
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
