a966a93d59e9c8e3f04826f2df833635b3d99e52
Tejas <tejasspatell2@gmail.com>
2026-06-15 20:53:30 +0530

    correct my backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  uri: DATABASE_URL,
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
